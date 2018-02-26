2128ac4bf59ba9cd29cd714a74d9952738cb2080
Andrés Artavia <andresartavia@Andress-MacBook-Pro.local>
2018-02-25 23:37:00 -0600

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,79 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img src="./assets/logo_beta.png" class="logo" />
+    <!-- <img src="./assets/Mockup_plantitaugh.png" class="mockups"> -->
+    <img src="./assets/iPhoneX.png" class="mockups iphone" />
+    <img src="./assets/samsungS8.png" class="mockups samsung" />
+    <div class="download">
+      <img src="./assets/ios.png" class="ios" />
+      <img src="./assets/android.png" class="android" />
+    </div>
+    <!-- <SocialNetwork /> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SocialNetwork from './components/SocialNetwork';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    SocialNetwork
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  .download {
+    width: 100%;
+    display: inline-block;
+  }
+
+  .ios {
+    width: 140px;
+  }
+
+  .android {
+    width: 140px;
+  }
+
+  .logo {
+    width: 100px;
+  }
+
+  .mockups {
+    width: 90%;
+    position: absolute;
+  }
+
+  .iphone {
+    left: 0;
+    z-index: 100;
+    top: 20vh;
+    animation: effect 2s 1s infinite linear;
+  }
+
+  .samsung {
+    left: 20vw;
+    top: 30vh;
+    animation: effect 2s 1s infinite linear;
+  }
+
+  @keyframes effect {
+    0%, 100% {
+      transform:translateY(0);
+    }
+    40% {
+      transform:translateY(-15px);
+    }
+  }
 </style>
---
+++ src/components/SocialNetwork.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="footer">
+        <a href="https://facebook.com/iAndrezx" target="_blank">
+            <Icon>
+                <icon name="facebook-square" scale="1.8" />
+            </Icon>
+        </a>
+        <a href="https://instagram.com/artaviandres" target="_blank" class="margin">
+            <Icon>
+                <icon name="instagram" scale="1.8" />
+            </Icon>
+        </a>
+        <a href="https://twitter.com/Artvavia" target="_blank" class="margin">
+            <Icon>
+                <icon name="twitter-square" scale="1.8" />
+            </Icon>
+        </a>
+        <a href="https://www.linkedin.com/in/artaviandres/" target="_blank" class="margin">
+            <Icon>
+                <icon name="linkedin" scale="1.8" />
+            </Icon>
+        </a>
+    </div>
+</template>
+
+<script>
+    import Icon from 'vue-awesome/components/Icon'
+    import 'vue-awesome/icons/facebook-square'
+    import 'vue-awesome/icons/instagram'
+    import 'vue-awesome/icons/twitter-square'
+    import 'vue-awesome/icons/linkedin'
+
+    export default {
+        name: "SocialNetwork",
+        components: {
+            Icon
+        }
+    }
+</script>
+
+<style scoped>
+    .footer {
+        position: absolute;
+        bottom: 5px;
+        left: 0;
+        width: 15%;
+    }
+    a {
+        cursor: pointer;
+        text-decoration: none;
+        color: rgba(0, 0, 0, 0.8);
+    }
+    .margin {
+        margin-left: 5%;
+    }
+</style>
